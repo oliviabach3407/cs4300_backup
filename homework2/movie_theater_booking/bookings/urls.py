be1bbb2081c443811976c8a39656b05d50378c3d
oliviabach3407 <oliviabach3407@gmail.com>
2025-02-13 14:32:30 -0700

    Creating template-based views and linking them to templates. These are separate from the viewset views, which are acting similarly to the admin interface. Not all the views are working as expected at this point.

--- homework2/movie_theater_booking/bookings/urls.py
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, SeatViewSet, BookingViewSet
+from .views import MovieViewSet, SeatViewSet, BookingViewSet, UserViewSet, view_movies, book_seat, booking_history
 from django.contrib.auth import views as auth_views
 
 #API Endpoints:
@@ -9,7 +9,11 @@ router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
 router.register(r'seats', SeatViewSet)
 router.register(r'bookings', BookingViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),  #API Endpoints
+    path('', view_movies, name="movies"),
+    path('book-seat/<int:movie_id>', book_seat, name="book_seat"),
+    path('booking-history/', booking_history, name="booking_history"),
 ]
--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from .models import Movie, Seat, Bookings
-from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
+from django.template.response import TemplateResponse
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Movie, Seat, Bookings, User
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer, UserSerializer
+
+
+############
+# Viewsets #
+############
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     """ Provides CRUD operations for movies. """
@@ -19,7 +29,7 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingSerializer
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request):
         """ Ensures seat isn't already booked before creating a booking. """
         movie_id = request.data.get('movie')
         seat_id = request.data.get('seat')
@@ -44,3 +54,43 @@ class BookingViewSet(viewsets.ModelViewSet):
         if user_id:
             return Bookings.objects.filter(user_id=user_id)
         return super().get_queryset()
+    
+########################
+# Template-based Views #
+########################
+'''
+    path('movies/', view_movies.asView(), name="movies"),
+    path('book-seat/', book_seat.asView(), name="book-seat"),
+    path('booking-history/', booking_history.asView(), name="booking-history"),
+'''
+
+def view_movies(request):
+    movies = Movie.objects.all() #gets all movie objects that exist
+
+    return TemplateResponse(request, 'movie_list.html', {"movies": movies})
+
+def book_seat(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    users = User.objects.all()
+    seats = Seat.objects.filter(is_booked=False)
+
+    if request.method == "GET":
+        return render(request, 'seat_booking.html', {'movie': movie, 'seats': seats, 'users': users})
+    
+    if request.method == 'POST':
+        seat_number = request.Post.get('seat_number')
+        seat = get_object_or_404(Seat, seat_number=seat_number)
+        user_name = request.POST.get('user_name')
+        user = get_object_or_404(User, name=user_name)
+
+        Bookings.objects.create(movie=movie, seat=seat, user=user)
+
+        seat.is_booked = True
+        seat.save()
+
+        return redirect('booking_history')
+    
+def booking_history(request, user_id):
+    bookings = Bookings.objects.all()
+
+    return render(request, 'booking_history.html', {"bookings": bookings})
